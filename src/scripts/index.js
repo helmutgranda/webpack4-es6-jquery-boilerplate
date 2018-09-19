733bf6952db576fd8fa973d878ac042483ef03e4
Helmut Granda <helmutgranda@Helmuts-MacBook-Pro.local>
2018-09-18 23:50:05 -0500

    Adding a test for jquery

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,7 @@
+import $ from 'jquery';
 import { qs, qsa, $on, $delegate } from './utils';
 
 import '../stylesheets/style.scss';
 
-console.log('Hello world');
+console.log('Hello worldsss');
+console.log($(".my-plugin"));
